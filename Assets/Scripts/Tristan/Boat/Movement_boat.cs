ef817362089f5f8317863c18c6d50acd259519de
Tristan <t.laurent@rubika-edu.re>
2025-12-25 19:34:52 +0400

    ajout particule

--- Assets/Scripts/Tristan/Boat/Movement_boat.cs
+++ Assets/Scripts/Tristan/Boat/Movement_boat.cs
@@ -6,7 +6,9 @@ public class Movement_boat : MonoBehaviour
     float speed = 5f;
     float speed_rotation = 25f;
 
- 
+    //recuperer particule
+    [SerializeField] GameObject particle;
+
     void Update()
     {
         //appelle de la fonction deplacement
@@ -24,5 +26,23 @@ public class Movement_boat : MonoBehaviour
         // Rotation gauche / droite
         float rotation = Input.GetAxis("Horizontal") * speed_rotation * Time.deltaTime;
         transform.Rotate(Vector3.up * rotation);
+
+        //on active si il y a un deplacement les particules
+        if (move != 0)
+        {
+            particle.SetActive(true);
+        }
+
+        //on active si il y a une rotation les particules
+        if (rotation != 0)
+        {
+            particle.SetActive(true);
+        }
+
+        //si il y a aucun mouvement on desactive
+        else if(move ==0 && rotation == 0)
+        {
+            particle.SetActive(false);
+        }
     }
 }
